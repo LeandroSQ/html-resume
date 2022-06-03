6e7c80377bffc0e85e360d3e3a8c27d548802c9c
LeandroSQ <11012599+LeandroSQ@users.noreply.github.com>
2022-06-03 05:17:02 +0000

    Deploying to gh-pages from @ LeandroSQ/html-resume@098c4e75cf42517c2cf4796b423ca63b9bcfe0d7 🚀

---
+++ landing.js
@@ -0,0 +1,2 @@
+function downloadPDF(){window.open("resume.pdf");const e=document.createElement("a");e.href="resume.pdf",e.download="Leandro Quevedo - resume.pdf",document.body.appendChild(e),e.click(),document.body.removeChild(e)}
+//# sourceMappingURL=landing.js.map
